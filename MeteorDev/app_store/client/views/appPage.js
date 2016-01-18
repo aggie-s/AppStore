b478d4d7bd847eba2a3990d638468c8a77666785
Dehong Song <dehongsong321@gmail.com>
2016-01-17 16:03:12 -0800

    Meteor

---
+++ MeteorDev/app_store/client/views/appPage.js
@@ -0,0 +1,11 @@
+Template.appPage.helpers({
+    getSuggestedApp: function(appId){
+        Meteor.subscribe('singleAppByAppId',appId);
+        return Apps.findOne({app_id:appId});
+    }
+});
+Template.appPage.events({
+    "click #backLink": function(evt){
+        history.back();
+    }
+});
